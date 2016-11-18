883a0339edffd338742397cfd30825a3a695d1e5
Hesai <oda_san@hotmail.com>
2016-11-18 11:53:51 -0800

    added logic code to main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,18 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else {
+	aler("Sorry try again.");
+}
+if (cardTwo === cardOne) {
+	alert("You found a match!");
+} else {
+	alert("Sorry try again.");
+}
+if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry try again.");
+}
